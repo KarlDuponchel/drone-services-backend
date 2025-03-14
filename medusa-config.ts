1f35aded6d3fd64a130a4aea24cc83563d525ca8
Karl <karl.d@snt2.fr>
2025-03-14 15:58:52 +0100

    backend

--- medusa-config.ts
+++ medusa-config.ts
@@ -11,6 +11,10 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+    cors: {
+      origin: ['http://localhost:3000'],
+      credentials: true
+    },
   }
 })
